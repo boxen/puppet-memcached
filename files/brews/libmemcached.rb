a7707382a52e87ba8b858ed9ccf379e00ef0ce66
Pierre Dulac <dulacpier@gmail.com>
2014-03-13 00:35:12 +0100

    Update the libmemcached formula

--- files/brews/libmemcached.rb
+++ files/brews/libmemcached.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Libmemcached < Formula
   homepage "http://libmemcached.org"
   url "https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz"
   sha1 "8be06b5b95adbc0a7cb0f232e237b648caf783e1"
   version '1.0.18-boxen1'
-  
+
   bottle do
     cellar :any
     sha1 "abc6b08082ef18c8abb0df901063e88ee21fa82e" => :mavericks
@@ -21,21 +21,61 @@ class Libmemcached < Formula
     depends_on "memcached"
   end
 
-  def patches
-    # https://bugs.launchpad.net/libmemcached/+bug/1284765
-    DATA
-  end
-
   def install
     ENV.append_to_cflags "-undefined dynamic_lookup" if MacOS.version <= :leopard
 
     args = ["--prefix=#{prefix}"]
 
     if build.with? "sasl"
-      args << "--with-memcached-sasl=#{Formula["memcached"].bin}/memcached"
+      args << "--with-memcached-sasl=#{Formula.factory("memcached").bin}/memcached"
     end
 
     system "./configure", *args
     system "make install"
   end
+
+  def patches
+    DATA
+  end
+
 end
+
+__END__
+diff --git a/clients/memflush.cc b/clients/memflush.cc
+index 8bd0dbf..cdba743 100644
+--- a/clients/memflush.cc
++++ b/clients/memflush.cc
+@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
+ {
+   options_parse(argc, argv);
+
+-  if (opt_servers == false)
++  if (*opt_servers != NULL)
+   {
+     char *temp;
+
+@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
+       opt_servers= strdup(temp);
+     }
+
+-    if (opt_servers == false)
++    if (*opt_servers != NULL)
+     {
+       std::cerr << "No Servers provided" << std::endl;
+       exit(EXIT_FAILURE);
+diff --git a/libmemcached-1.0/memcached.h b/libmemcached-1.0/memcached.h
+index bc16e73..dcee395 100644
+--- a/libmemcached-1.0/memcached.h
++++ b/libmemcached-1.0/memcached.h
+@@ -43,7 +43,11 @@
+ #endif
+
+ #ifdef __cplusplus
++#ifdef _LIBCPP_VERSION
+ #  include <cinttypes>
++#else
++#  include <tr1/cinttypes>
++#endif
+ #  include <cstddef>
+ #  include <cstdlib>
+ #else
