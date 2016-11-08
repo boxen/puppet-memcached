db1a774c5d181e3b28015e491d1f918dadd0d2ea
Orien Madgwick <_@orien.io>
2016-11-08 11:37:13 +1100

    Use operatingsystem rather than osfamily

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,7 +10,7 @@ class memcached::service(
     default => stopped,
   }
 
-  if $osfamily == 'Darwin' {
+  if $::operatingsystem == 'Darwin' {
     service { 'com.boxen.memcached':
       ensure => stopped,
       enable => false,
