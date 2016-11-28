58ac9e6bb499365fe2f33ff823bb23dbc7c55320
Brian Stengaard <brian@stengaard.eu>
2016-11-28 14:14:10 -0500

    use correct sha256 sum
    previous was sum of redirect page from launchpad. EEBCK.

--- files/brews/libmemcached.rb
+++ files/brews/libmemcached.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libmemcached < Formula
   homepage 'http://libmemcached.org'
   url 'https://launchpad.net/libmemcached/1.0/1.0.15/+download/libmemcached-1.0.15.tar.gz'
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 'dd7e9560029835bddf761a5b4c2339d9e5c7374558659b6c11b2c95e7d3a4325'
   version '1.0.15-boxen1'
 
   depends_on 'memcached'
