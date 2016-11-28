ea66c2650285fa1808965b5c0ffec8df19b283c8
Brian Stengaard <brian@stengaard.eu>
2016-11-28 14:02:03 -0500

    use sha256 checksum for libmemcached

--- files/brews/libmemcached.rb
+++ files/brews/libmemcached.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libmemcached < Formula
   homepage 'http://libmemcached.org'
   url 'https://launchpad.net/libmemcached/1.0/1.0.15/+download/libmemcached-1.0.15.tar.gz'
-  sha1 'd49ceec8efcc13c2a6ab15416d9caeeeeaa49a48'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   version '1.0.15-boxen1'
 
   depends_on 'memcached'
